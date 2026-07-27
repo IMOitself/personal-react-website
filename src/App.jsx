7bb24a46e87bc8d7c48d9361256ce7d4fa0b1634
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-27 08:33:28 +0800

    fix: visible white background on smaller screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 export default function App() {
 
   return (
-    <div style={{backgroundColor:"#2B2D5B", height:"100vh", textAlign:"center", paddingTop:"25px"}}>
+    <div style={{height:"100vh", textAlign:"center", paddingTop:"25px"}}>
       <img src={"https://raw.githubusercontent.com/IMOitself/personal-github-banner/master/banner-main.svg"}/>
       <br/>
       <img src={"https://raw.githubusercontent.com/IMOitself/personal-github-banner/master/banner-recent-repo.svg"}/>
